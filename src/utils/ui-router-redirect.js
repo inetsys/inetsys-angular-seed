ff35e97a01bd916b6fb91b8f2c9aeabc24e70bf8
Luis Lafuente <luis.lafuente@inetsys.es>
2016-02-25 12:02:24 +0100

    ui router redirectTo

---
+++ src/utils/ui-router-redirect.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// redirect to a new state base on: redirectTo in state definition
+// example:
+// $stateProvider
+// .state("xx", {
+//   url: "/xx",
+//   redirectTo: "xx.yy"
+// });
+
+angular
+.module('app')
+.run(['$rootScope', '$state', function($rootScope, $state) {
+
+    $rootScope.$on('$stateChangeSuccess', function(evt, to, params) {
+      if (to.redirectTo) {
+        evt.preventDefault();
+        $state.go(to.redirectTo, params)
+      }
+    });
+}]);
